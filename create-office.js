c2319ea25d813b6a1875b9d49dc5acd2e8d05bba
Tony Bowden <tony@tmtm.com>
2021-08-13 14:36:52 +0300

    Update create-office.js to optionally link a Ministry

--- create-office.js
+++ create-office.js
@@ -3,7 +3,20 @@ const fs = require('fs');
 let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
-module.exports = (label) => {
+module.exports = (label, ministry) => {
+  claims = {
+    P31:   { value: 'Q294414' }, // instance of: public office
+    P279:  { value: 'Q83307'  }, // subclas of: minister
+    P17:   { value: meta.country ? meta.country.id : meta.jurisdiction.id },
+    P1001: { value: meta.jurisdiction.id },
+    P361: { // part of
+      value: meta.cabinet.parent,
+      references: { P854: meta.source.url },
+    }
+  }
+
+  if(ministry)   claims['P2389']  = ministry
+
   return {
     type: 'item',
     labels: {
@@ -12,15 +25,6 @@ module.exports = (label) => {
     descriptions: {
       en: `cabinet position in ${meta.jurisdiction.name}`,
     },
-    claims: {
-      P31:   { value: 'Q294414' }, // instance of: public office
-      P279:  { value: 'Q83307'  }, // subclas of: minister
-      P17:   { value: meta.country ? meta.country.id : meta.jurisdiction.id },
-      P1001: { value: meta.jurisdiction.id },
-      P361: { // part of
-        value: meta.cabinet.parent,
-        references: { P854: meta.source.url },
-      }
-    }
+    claims: claims
   }
 }
