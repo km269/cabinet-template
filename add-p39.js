eb5b79f25ea0282703ff8a517cba0daae0e500ac
Tony Bowden <tony@tmtm.com>
2021-12-18 16:09:14 +0200

    Allow add-p39 script to take an end date

--- add-p39.js
+++ add-p39.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
-module.exports = (id, position, startdate) => {
+module.exports = (id, position, startdate, enddate) => {
   qualifier = {
     P580: meta.cabinet.start,
   }
 
-  if(startdate)      qualifier['P580']  = startdate
+  if(startdate) qualifier['P580']  = startdate
+  if(enddate)   qualifier['P582']  = enddate
 
   return {
     id,
